101fc90ab9662ba8c9dee16664d355442fd9f00e
longdv ph29867 <emchuaroinhe@gmail.com>
2023-06-18 22:18:37 +0700

    update

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -30,7 +30,7 @@ export default function Skills() {
     {
       icon: <SlSupport />,
       title: "FOREIGN LANGUAGE",
-      desc: ["English: intermediate"],
+      desc: ["English: intermediate", "Japanese: N5"],
     },
     {
       icon: <FiUsers />,
