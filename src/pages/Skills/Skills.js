46e03020ad6818d558ab3e890ac79ab343f20a5a
longdv ph29867 <emchuaroinhe@gmail.com>
2024-02-17 12:25:38 +0700

    update

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import Title from '../../components/Title/Title'
-import {SlAnchor, SlEqualizer, SlMicrophone, SlLayers, SlSupport} from 'react-icons/sl';
-import {FiUsers} from 'react-icons/fi';
-import styles from './skills.module.scss'
-import ItemSkill from '../../components/ItemSkill/ItemSkill';
-import imgHome03 from '../../assets/home_03.png'
+import React from "react";
+import Title from "../../components/Title/Title";
+import {
+  SlAnchor,
+  SlEqualizer,
+  SlMicrophone,
+  SlLayers,
+  SlSupport,
+} from "react-icons/sl";
+import { FiUsers } from "react-icons/fi";
+import styles from "./skills.module.scss";
+import ItemSkill from "../../components/ItemSkill/ItemSkill";
+import imgHome03 from "../../assets/home_03.png";
 export default function Skills() {
   const skills = [
     {
@@ -30,23 +36,29 @@ export default function Skills() {
     {
       icon: <SlSupport />,
       title: "FOREIGN LANGUAGE",
-      desc: ["English: intermediate", "Japanese: N5"],
+      desc: [
+        "English: intermediate",
+        // "Japanese: N5",
+      ],
     },
     {
       icon: <FiUsers />,
       title: "KNOWLEDGE",
-      desc: ["Good understanding about the Agile and Scrum process", "Good time management, presentation and teamwork skills"],
+      desc: [
+        "Good understanding about the Agile and Scrum process",
+        "Good time management, presentation and teamwork skills",
+      ],
     },
-  ]
+  ];
 
   return (
-    <div style={{paddingTop: 70}} id='skills'>
+    <div style={{ paddingTop: 70 }} id="skills">
       <div className={styles.skills}>
         <div className="container">
-          <Title className={styles.title} title={'Skills'}/>
+          <Title className={styles.title} title={"Skills"} />
           <div className={styles.skillsContainer}>
             {skills.map((item, index) => {
-              return <ItemSkill key={index} item={item}/>
+              return <ItemSkill key={index} item={item} />;
             })}
           </div>
         </div>
@@ -55,5 +67,5 @@ export default function Skills() {
         </div>
       </div>
     </div>
-  )
+  );
 }
