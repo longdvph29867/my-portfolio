9ec550e6c8ee9955407a2d6efd64a17ad231efb2
longdv ph29867 <emchuaroinhe@gmail.com>
2024-02-17 12:35:12 +0700

    update

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import ItemProject from "../../components/ItemProject/ItemProject";
 import Title from "../../components/Title/Title";
 import styles from "./projects.module.scss";
-import productsManagement from '../../assets/productsManagement.png'
-import shoppingCart from '../../assets/shoppingCart.png'
-import movieTickets from '../../assets/movieTickets.png'
-import vlearning from '../../assets/vlearning.png'
-import adminVlearning from '../../assets/adminVlearning.png'
-import samar from '../../assets/samar.png'
+import productsManagement from "../../assets/productsManagement.png";
+import shoppingCart from "../../assets/shoppingCart.png";
+import movieTickets from "../../assets/movieTickets.png";
+import vlearning from "../../assets/vlearning.png";
+import adminVlearning from "../../assets/adminVlearning.png";
+import samar from "../../assets/samar.png";
 
 export default function Projects() {
   const projectList = [
@@ -24,7 +24,7 @@ export default function Projects() {
     {
       type: "Movie Tickets",
       img: movieTickets,
-      link: "https://booking-movie-tickets.vercel.app/",
+      link: "https://booking-tickets-navy.vercel.app/",
     },
     {
       type: "Shopping Cart",
@@ -43,13 +43,13 @@ export default function Projects() {
     },
   ];
   return (
-    <div style={{paddingTop: 70}} id='projects'>
+    <div style={{ paddingTop: 70 }} id="projects">
       <div className={styles.projects}>
         <div className="container">
-          <Title title='ProJects' className={styles.title}/>
+          <Title title="ProJects" className={styles.title} />
           <div className={styles.projectContainer}>
             {projectList.map((item, index) => {
-              return <ItemProject key={index} item={item}/>
+              return <ItemProject key={index} item={item} />;
             })}
           </div>
         </div>
